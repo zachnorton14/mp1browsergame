61939e310c35a8f4980a8bfad6ac9b7dd12e601e
okayzach <znortyy@gmail.com>
2022-04-20 21:51:11 -0400

    finished the project

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // generates/comprises the 5 letter "wordle"
+let wordle;
+
+window.onload = () => {
+    wordle = getWordle()
+    console.log(wordle)
+}
 
 async function getWordle(){
     const response = await fetch('/assets/wordle-answers-alphabetical.txt')
@@ -7,7 +13,6 @@ async function getWordle(){
     let x = Math.floor(Math.random() * wordList.length);
     return wordle = wordList[x]    
 }
-
 // changes the tile being typed in based on amount of keys pressed
 
 let currentTile = '.tile1 p';
@@ -59,6 +64,7 @@ function keyboardEvent(){
             removeFromCounter()
         } else if (`${e.code}` === 'Enter'){
             enterCounter()
+            compareWordle()
         } else {
             console.log('That key does not exist')
         }
@@ -77,6 +83,7 @@ deleteButton.addEventListener('click', () => {
 let enterButton = document.querySelector('.enter')
 enterButton.addEventListener('click', () => {
     enterCounter()
+    compareWordle()
 })
 
 // invoking the keyboard functions
@@ -109,9 +116,65 @@ createKey('.x', 'X')
 createKey('.y', 'Y')
 createKey('.z', 'Z')
 
-// changing the color of the tiles based on the "wordle"
+/* gathering the tiles based on the count, when enter is pressed, and changing the 
+color of the tiles based on the "wordle" */
 
-async function compareWordle(){
-    let wordle = await getWordle()
-    
+
+
+function compareWordle(){
+    row = {
+        tile1: {
+            location: document.querySelector('.tile' + (count - 4)),
+            text: document.querySelector('.tile' + (count - 4) + ' p').textContent.toLowerCase()
+        },
+        tile2: {
+            location: document.querySelector('.tile' + (count - 3)),
+            text: document.querySelector('.tile' + (count - 3) + ' p').textContent.toLowerCase()
+        },
+        tile3: {
+            location: document.querySelector('.tile' + (count - 2)),
+            text: document.querySelector('.tile' + (count - 2) + ' p').textContent.toLowerCase()
+        },
+        tile4: {
+            location: document.querySelector('.tile' + (count - 1)),
+            text: document.querySelector('.tile' + (count - 1) + ' p').textContent.toLowerCase()
+        },
+        tile5: {
+            location: document.querySelector('.tile' + count),
+            text: document.querySelector('.tile' + count + ' p').textContent.toLowerCase()
+        }
+    }
+
+    function scanTileText(text, location, tilenum){
+        userGuess = row.tile1.text + row.tile2.text + row.tile3.text + row.tile4.text + row.tile5.text
+        tileLocation = location
+        if (wordle.match(text)){
+            if(wordle[tilenum] === text){
+                tileLocation.style.backgroundColor = '#538d4e'
+                tileLocation.style.borderColor= '#538d4e'
+                document.querySelector('.' + text).style.backgroundColor = '#538d4e'
+            } else {
+                tileLocation.style.backgroundColor = '#b59f3b'
+                tileLocation.style.borderColor = '#b59f3b'
+                document.querySelector('.' + text).style.backgroundColor = '#b59f3b'
+            }
+        } else {
+            tileLocation.style.backgroundColor = '#3a3a3c'
+            document.querySelector('.' + text).style.backgroundColor = '#3a3a3c'
+        }
+        if (wordle === userGuess){
+            document.querySelector('.h1').textContent = 'You Win'
+        }
+        if (count === 30){
+            if(wordle != userGuess)
+            document.querySelector('.h1').textContent = 'You Lose'
+        }
+
+    }
+
+    scanTileText(row.tile1.text, row.tile1.location, 0)
+    scanTileText(row.tile2.text, row.tile2.location, 1)
+    scanTileText(row.tile3.text, row.tile3.location, 2)
+    scanTileText(row.tile4.text, row.tile4.location, 3)
+    scanTileText(row.tile5.text, row.tile5.location, 4)
 }
